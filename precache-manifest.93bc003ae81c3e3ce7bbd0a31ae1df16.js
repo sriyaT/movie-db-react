543e6914c28b401ecb7ed9678bc2582de60342ee
sriyaT <tsriya6@gmail.com>
2020-06-15 15:24:08 +0530

    Updates

---
+++ precache-manifest.93bc003ae81c3e3ce7bbd0a31ae1df16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "616e20bf2b00d42837b61df411efc419",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9ff2ebee8f35d30db63",
+    "url": "/static/css/main.da7a35a4.chunk.css"
+  },
+  {
+    "revision": "0b7f5f1d0c07d3bdef8f",
+    "url": "/static/js/2.814b0953.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.814b0953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9ff2ebee8f35d30db63",
+    "url": "/static/js/main.6c09375e.chunk.js"
+  },
+  {
+    "revision": "64acf199eeac4c1a8278",
+    "url": "/static/js/runtime-main.bdde1b26.js"
+  }
+]);
